698622699ca5cf1c962f76a72082f0d85bb8cf83
Andrej Juric <andrej.juric991@gmail.com>
2023-04-12 22:19:49 +0200

    2commit

--- src/Components/DetailsMake.js
+++ src/Components/DetailsMake.js
@@ -1,29 +1,40 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const DetailsMake = () => {
   const { makeid } = useParams();
+  console.log(makeid);
 
   const [makeData, setMakeData] = useState({});
 
   useEffect(() => {
-    fetch("https://api.baasic.com/v1/sata/resources/vehicleMakes/" + makeid, {
+    fetch("https://api.baasic.com/v1/sata/resources/vehicleMakes" + makeid, {
       headers: {
         "X-BAASIC-API-KEY": "sata",
       },
     })
-      .then((resp) => {
-        return resp.json();
+      .then((res) => {
+        return res.json();
       })
       .then((resp) => {
-        setMakeData(resp.item);
+        setMakeData(resp);
         console.log(resp);
       })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      {makeData && (
+        <h1>
+          <Link to="/">Go back to home page</Link>
+          Vehicle name is: {makeData.name} ({makeData.id})
+        </h1>
+      )}
+    </div>
+  );
 };
 
 export default DetailsMake;
